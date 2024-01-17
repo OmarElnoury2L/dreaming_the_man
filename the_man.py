8b4bacabe2cd52ab8932ced22cc874919171734c
unknown <omar.elnoury@liceoeinsteinmilano.edu.it>
2024-01-17 17:06:32 +0100

    Aggiunta classe Vettore con attributi 'x1' e 'y1' e moduli 'modulo()' e 'somma()'

--- the_man.py
+++ the_man.py
@@ -1,1 +1,15 @@
-print("dreaming...")
+class Vettore:
+    def __init__(self,x1,y1):
+        self.x1 = x1
+        self.y1 = y1
+    def modulo(self):
+        m = (((self.x1)**2+(self.y1)**2)**(1/2))
+        return m
+    def somma(self, v2):
+        v2.x1 = self.x1+v2.x1
+        v2.y1 = self.y1+v2.y1
+        return v2
+v1 = Vettore(5,3)
+v2 = Vettore(-1,2)
+print(v1.modulo())
+print(v1.somma(v2))
