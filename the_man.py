51d8a9d0052c045fcec4f66bed52e170a1eed702
unknown <omar.elnoury@liceoeinsteinmilano.edu.it>
2024-01-24 15:39:32 +0100

    sistemati i print()

--- the_man.py
+++ the_man.py
@@ -8,8 +8,8 @@ class Vettore:
     def somma(self, v2):
         v2.x1 = self.x1+v2.x1
         v2.y1 = self.y1+v2.y1
-        return v2
+        return v2 
 v1 = Vettore(5,3)
-v2 = Vettore(-1,2)
-print(v1.modulo())
-print(v1.somma(v2))
+v2 = Vettore(-2,1)
+v2 = v1.somma(v2)
+print(v2.modulo())
